5693ac23024805d5b576e38779293df531751160
mohammed123333 <54069664+mohammed123333@users.noreply.github.com>
2025-09-04 14:05:35 +0300

    Updated doctorData.ts

--- src/data/doctorData.ts
+++ src/data/doctorData.ts
@@ -206,6 +206,7 @@ drnazihjallad: {
   email: 'drnazihjallad@example.com',
   phone: '+962 7 9794 2027',
   priceClinic: 30,
+  priceHome: --,
   waitingTime: '30',
   location: { ar: 'عمّان - الدوار الخامس', en: 'Amman - Fifth Circle' },
   ar: {
