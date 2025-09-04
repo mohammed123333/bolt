de09bc8fcb3f38c408a8b4ad0839bc0be72702b4
mohammed123333 <54069664+mohammed123333@users.noreply.github.com>
2025-09-04 12:50:30 +0300

    Updated Doctors.tsx

--- src/components/Doctors.tsx
+++ src/components/Doctors.tsx
@@ -260,7 +260,7 @@ const scroll = (direction: 'left' | 'right') => {
           </div>
 
           {/* Scroll Indicators */}
-          <div className="flex justify-center mt-8 space-x-27space-x-reverse">
+          <div className="flex justify-center mt-8 space-x-4 space-x-reverse">
             {Array.from({ length: Math.ceil(doctors.length / 3) }).map((_, index) => (
               <div
                 key={index}
