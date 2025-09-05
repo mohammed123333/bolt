8cc02a8d553979cc8692f293d873785e595ed8df
mohammed123333 <54069664+mohammed123333@users.noreply.github.com>
2025-09-05 12:15:48 +0300

    Updated PersonalInfoPage.tsx

--- src/pages/PersonalInfoPage.tsx
+++ src/pages/PersonalInfoPage.tsx
@@ -5,6 +5,8 @@ import { useLanguage } from '../contexts/LanguageContext';
 import { doctorData, insuranceCompanies } from '../data/doctorData';
 import LanguageToggle from '../components/LanguageToggle';
 import emailjs from '@emailjs/browser';
+import React, { useState, useEffect } from 'react';
+
 
 const PersonalInfoPage = () => {
   const { doctorId } = useParams<{ doctorId: string }>();
